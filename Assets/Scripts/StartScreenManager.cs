5e3624d1d17b4f5a08d2d29d3660b9e0ae54e020
TwistdRabbit <68243127+TwistdRabbit@users.noreply.github.com>
2024-03-20 18:44:07 +0000

    start screen and tutorial scenes added and linked

---
+++ Assets/Scripts/StartScreenManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject dinosaur;
+    private AudioSource audioSource;
+    public AudioClip[] roars;
+    private AudioClip roarClip;
+    private Animator animator;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = dinosaur.GetComponent<Animator>();
+    }
+
+    public void StartGame()
+    {
+        animator.SetTrigger("Roar");
+        roarClip = roars[UnityEngine.Random.Range(0, roars.Length)];
+        audioSource.clip = roarClip;
+        audioSource.Play();
+        Invoke("LoadScene", 4f);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
