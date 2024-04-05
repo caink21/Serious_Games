915408ff49d9728b96001b66b3e5829a6e774d94
caink21 <75377754+caink21@users.noreply.github.com>
2024-04-05 05:08:16 +0100

    gameover

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,15 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject imageCanvas;
+    public GameObject imageCanvas2;
+    public  Sprite Dinoimage;
+    public  Sprite Dinoimage2;
+  
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("GameOverScreen", LoadSceneMode.Additive);
+            
+            ShowImage();   
+        }
+    }
+
+    private void ShowImage()
+    {
+        if(imageCanvas != null)
+        {
+            Image imageComponent= imageCanvas.GetComponent<Image>();
+            if(imageComponent != null) 
+            {
+                imageComponent.sprite = Dinoimage;
+                imageCanvas.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("No Image");
+            }
+        }
+
+        if (imageCanvas2 != null)
+        {
+            Image imageComponent = imageCanvas2.GetComponent<Image>();
+            if (imageComponent != null)
+            {
+                imageComponent.sprite = Dinoimage2;
+                imageCanvas2.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("No Image");
+            }
         }
     }
 }
