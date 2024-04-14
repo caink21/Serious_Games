b79f99534c76e4a2716f826f745a51e582008ec0
caink21 <75377754+caink21@users.noreply.github.com>
2024-04-14 16:59:05 +0100

    update

--- Assets/Scripts/door.cs
+++ Assets/Scripts/door.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class door : MonoBehaviour
 {    
     public Animator doorAnim;
+    public bool isOpened = false;
     public GameObject objectToEnable;
     public float delayInSeconds = 1.5f;
 
     private bool triggered = false;
 
-    void OnTriggerEnter(Collider other)
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        doorAnim.ResetTrigger("close");
+    //        doorAnim.SetTrigger("open");
+    //        StartCoroutine(EnableObjectAfterDelay());
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isOpened)
         {
-            doorAnim.SetTrigger("open");
+            // Play the door opening animation
+            doorAnim.SetBool("IsOpen", true);
+            isOpened = true;
             StartCoroutine(EnableObjectAfterDelay());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && isOpened)
+        {
+            // Play the door closing animation
+            doorAnim.SetBool("IsOpen", false);
+            isOpened = false;
         }
     }
 
