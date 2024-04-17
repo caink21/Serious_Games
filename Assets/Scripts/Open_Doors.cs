149a292a7f6a438d167ec6d140bcac6590701a4a
TwistdRabbit <willdavidhamilton@gmail.com>
2024-04-17 18:22:06 +0100

    gameplay scene updated

--- Assets/Scripts/Open_Doors.cs
+++ Assets/Scripts/Open_Doors.cs
@@ -6,8 +6,11 @@ public class Open_Doors : MonoBehaviour
 {
     public Animator doorAnim;
     public bool isOpened = false;
-    public GameObject objectToEnable;
+    public GameObject objectToEnable = null;
     public float delayInSeconds = 1.5f;
+
+    [SerializeField] AudioClip dinoClip;
+    [SerializeField] AudioClip doorClip;
 
     private bool triggered = false;
 
@@ -18,7 +21,8 @@ public class Open_Doors : MonoBehaviour
         if (other.CompareTag("Player"))
             if (other.CompareTag("Player") && !isOpened)
             {
-                doorAnim.SetTrigger("open");
+                AudioSource.PlayClipAtPoint(doorClip, transform.position);
+                //doorAnim.SetTrigger("open");
                 // Play the door opening animation
                 doorAnim.SetBool("IsOpen", true);
                 isOpened = true;
@@ -38,9 +42,17 @@ public class Open_Doors : MonoBehaviour
 
     IEnumerator EnableObjectAfterDelay()
     {
-        // Wait for specified delay
-        yield return new WaitForSeconds(delayInSeconds);
-        // Enable the object
-        objectToEnable.SetActive(true);
+        if (objectToEnable == null)
+        {
+            yield return null;
+        }
+        else
+        {
+            AudioSource.PlayClipAtPoint(dinoClip, transform.position);            
+            // Wait for specified delay
+            yield return new WaitForSeconds(delayInSeconds);
+            // Enable the object
+            objectToEnable.SetActive(true);
+        }
     }
 }
